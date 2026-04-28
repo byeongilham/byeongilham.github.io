33814fde82a1bae6f7bf136ba73b57c9b5e50af8
byeongilham <robot21@kakao.com>
2026-04-28 06:10:00 +0000

    Deploying to gh-pages from @ byeongilham/byeongilham.github.io@90ffc24725069de12908390908265e075ff94562 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "Ongoing Projects",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
